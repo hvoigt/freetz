dccf686ba327ad2ceacf3108880285c6e32afb21
Heiko Voigt <hvoigt@hvoigt.net>
2016-01-25 17:31:02 +0100

    update iproute2 package to current kernel version (2.6.36)

--- make/iproute2/iproute2.mk
+++ make/iproute2/iproute2.mk
@@ -1,8 +1,8 @@
-$(call PKG_INIT_BIN, 2.6.16-060323)
-$(PKG)_SOURCE:=iproute2-$($(PKG)_VERSION).tar.gz
+$(call PKG_INIT_BIN, 2.6.36)
+$(PKG)_SOURCE:=v$($(PKG)_VERSION).tar.gz
 #$(PKG)_DIR:=iproute2-$($(PKG)_VERSION)/$($(PKG)_DIR)/tc
 #$(PKG)_SITE:=http://devresources.linux-foundation.org/dev/iproute2/download/ - currently down :(
-$(PKG)_SITE:=http://www.jzab.de/files/iproute2/
+$(PKG)_SITE:=https://github.com/hvoigt/iproute2/archive/
 $(PKG)_SOURCE_FILE:=$($(PKG)_DIR)/tc/tc.c
 $(PKG)_BINARY:=$($(PKG)_DIR)/tc/tc
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/sbin/tc
